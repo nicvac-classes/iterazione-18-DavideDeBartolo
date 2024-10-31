02f1e25f073aa2d27690d97cd66ad9e5d2a6f93a
DavideDeBartolo <davide.debartolo@marconibari.edu.it>
2024-10-31 10:27:05 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
-using namespace std;
+#include <vector>
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    std::vector<int> livelliAcqua(7);
+    int somma = 0;
+
+    std::cout << "Inserisci i livelli dell'acqua per ogni giorno della settimana (in cm):\n";
+    for (int i = 0; i < 7; ++i) {
+        std::cout << "Giorno " << (i + 1) << ": ";
+        std::cin >> livelliAcqua[i];
+        somma += livelliAcqua[i];
+    }
+
+    std::cout << "Giorni in cui il livello dell'acqua supera i 10 cm:\n";
+    for (int i = 0; i < 7; ++i) {
+        if (livelliAcqua[i] > 10) {
+            std::cout << "Giorno " << (i + 1) << "\n";
+        }
+    }
+
+    double livelloMedio = static_cast<double>(somma) / 7;
+    std::cout << "Valore medio del livello dell'acqua: " << livelloMedio << " cm\n";
+
+    if (livelloMedio > 0) {
+        std::cout << "Acqua alta\n";
+    } else {
+        std::cout << "Livello normale\n";
+    }
+
+    return 0;
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+ 
+ 
+
